05e46811305c84fa957d7af8fef27b782cfc73fb
Maxim Baryshnikov <baryshnikkov@gmail.com>
2022-08-12 22:08:00 +0300

    fix: перенесен весь JS код в файл index.js

--- scripts/accordeon.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.accordion__button').forEach(el => {
-    el.addEventListener('click', () => {
-        el.classList.toggle('accordion__button_close');
-        el.closest('.accordion__element').querySelector('.accordion__subtitle').classList.toggle('accordion__subtitle_opene');
-    });
-});
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.accordion__button').forEach(el => {
+  el.addEventListener('click', () => {
+      el.classList.toggle('accordion__button_close');
+      el.closest('.accordion__element').querySelector('.accordion__subtitle').classList.toggle('accordion__subtitle_opene');
+  });
+});
